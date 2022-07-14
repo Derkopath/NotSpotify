1fae831218fdefba092d13cf27122d156819fac0
Derkopath <derkopath@gmail.com>
2022-07-14 23:57:58 +0200

    Adding User class with the constructor.

---
+++ NotSpotify/main/Clases/user-class.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class User {
+    constructor(id, name, email, passwrd){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.passwrd = passwrd;
+    }
+    
+}
---
+++ NotSpotify/main/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+/*Introducir los usuarios mediante la clase */
+
+/* const user1 = new User */
